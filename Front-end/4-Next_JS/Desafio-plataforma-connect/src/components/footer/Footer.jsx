43c492fa699ffb8e69269875aabe8ad85e722f97
ViniciusGCP94 <vinniegcp@gmail.com>
2026-02-04 13:26:44 -0300

    feat(home, header, footer): finalizada home, header e footer responsivos todos os arquivos.

--- Front-end/4-Next_JS/Desafio-plataforma-connect/src/components/footer/Footer.jsx
+++ Front-end/4-Next_JS/Desafio-plataforma-connect/src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer()  {
   return (
     <>
         <footer className={S.footer}>
-            <span className={S.textFooter}>© 2025 Vai na Web & Empower. Todos os direitos reservados. Projeto destinado exclusivamente a fins educativos.</span>
+            <div className={S.textContainer}><span className={S.textCopyright}>© 2025 Vai na Web & Empower.</span><span className={S.textCopyright}>Todos os direitos reservados. Projeto destinado exclusivamente a fins educativos.</span></div>
         </footer>
     </>
   )
--- Front-end/4-Next_JS/Desafio-plataforma-connect/src/components/header/Header.jsx
+++ Front-end/4-Next_JS/Desafio-plataforma-connect/src/components/header/Header.jsx
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import S from "./header.module.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export default function Header() {
+function Header() {
   const [menuAberto, setMenuAberto] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    function menuMobile() {
+      setIsMobile(window.innerWidth <= 768);
+      if (window.innerWidth > 768) {
+        setMenuAberto(false);
+      }
+    }
+
+    window.addEventListener("resize", menuMobile);
+    return () => window.removeEventListener("resize", menuMobile);
+  }, []);
 
   return (
     <>
@@ -18,20 +31,22 @@ export default function Header() {
             />
           </Link>
         </section>
-        <nav className={S.nav}>
-          <Link className={S.link} to="/doacao">
-            Doação
-          </Link>
-          <Link className={S.link} to="/voluntariado">
-            Voluntariado
-          </Link>
-          <Link className={S.link} to="/mentoria">
-            Mentorias
-          </Link>
-          <Link className={S.link} to="/eventosEP">
-            Eventos
-          </Link>
-        </nav>
+        {!isMobile && (
+          <nav className={S.nav}>
+            <Link className={S.link} to="/doacao">
+              Doação
+            </Link>
+            <Link className={S.link} to="/voluntariado">
+              Voluntariado
+            </Link>
+            <Link className={S.link} to="/mentoria">
+              Mentorias
+            </Link>
+            <Link className={S.link} to="/eventosEP">
+              Eventos
+            </Link>
+          </nav>
+        )}
         <img
           className={S.imgUser}
           src="https://avatars.githubusercontent.com/u/118212495?v=4"
@@ -43,8 +58,43 @@ export default function Header() {
         <Link to={"/usuario"}>Vincius Bispo</Link>
         <Link>Meu Voluntariado</Link>
         <Link>Configurações de conta</Link>
-        <Link>Sair</Link>
+        {isMobile && (
+          <div>
+            <Link
+              className={S.link}
+              to="/doacao"
+              onClick={() => setMenuAberto(false)}
+            >
+              Doação
+            </Link>
+            <Link
+              className={S.link}
+              to="/voluntariado"
+              onClick={() => setMenuAberto(false)}
+            >
+              Voluntariado
+            </Link>
+            <Link
+              className={S.link}
+              to="/mentoria"
+              onClick={() => setMenuAberto(false)}
+            >
+              Mentorias
+            </Link>
+            <Link
+              className={S.link}
+              to="/eventosEP"
+              onClick={() => setMenuAberto(false)}
+            >
+              Eventos
+            </Link>
+          </div>
+        )}
+
+        <Link onClick={() => setMenuAberto(false)}>Sair</Link>
       </nav>
     </>
   );
 }
+
+export default Header;

0aa12bc18f142a28f9f063054cbb936dff3a7e18
ViniciusGCP94 <vinniegcp@gmail.com>
2026-08-16 18:54:05 -0300

    publicando aprendizado backend

--- Back-end/Z. desafios/API-voluntarios/src/middlewares/validators/inscricoesValidator.js
+++
@@ -1,7 +0,0 @@
-const {body} = require('express-validator');
-
-const validarInscricaoNaAcao = [
-    body('id_acao').isInt().withMessage('O ID da ação deve ser um número inteiro.'),
-];
-
-module.exports = {validarInscricaoNaAcao}
